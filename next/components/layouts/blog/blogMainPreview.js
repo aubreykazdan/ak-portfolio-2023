207963c2ab353f1b81466596ff52743f12125e6e
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2023-03-12 14:19:43 -0400

    new hover effects on blog preview

--- next/components/layouts/blog/blogMainPreview.js
+++ next/components/layouts/blog/blogMainPreview.js
@@ -37,14 +37,8 @@ export default function BlogMainPreview({ data, showHeading }) {
 
             <div className=" h-full w-full lg:absolute lg:inset-y-0 lg:-right-[41%] xl:-right-[70%]">
               <div className="h-full flex-col items-center justify-center lg:flex">
-                <div className="lg:max-w-xl bg-white shadow-lg p-8">
-                  <div className="text-base">
-                    <time className="">
-                      {new Date(publishedAt).toLocaleDateString(
-                        "en-us",
-                        options
-                      )}
-                    </time>
+                <Link href={`blog/${slug}`}>
+                  <div className="lg:max-w-xl transition-animate hover:bg-accent-light bg-white shadow-lg p-8">
                     <div className="category mt-2">
                       {categories.map((item) => {
                         return (
@@ -54,12 +48,18 @@ export default function BlogMainPreview({ data, showHeading }) {
                         );
                       })}
                     </div>
+                    <h3 className="my-2">{title}</h3>
+                    <time>
+                      {new Date(publishedAt).toLocaleDateString(
+                        "en-us",
+                        options
+                      )}
+                    </time>
+                    <div className="mt-2">
+                      <BlogContent content={description} />
+                    </div>
                   </div>
-                  <h3 className="mt-2">{title}</h3>
-                  <div className="mt-2">
-                    <BlogContent content={description} />
-                  </div>
-                </div>
+                </Link>
               </div>
             </div>
           </div>
@@ -67,8 +67,4 @@ export default function BlogMainPreview({ data, showHeading }) {
       </Container>
     </div>
   );
-}
-
-{
-  /* <Link href={`blog/${slug}`}></Link> */
 }
